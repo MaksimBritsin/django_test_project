a1162f5ea570f4a880a907326ee7f7f78069af86
MaksimBritsin <111296865+MaksimBritsin@users.noreply.github.com>
2024-11-19 08:30:38 +0300

    Second

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.http import (HttpResponseNotFound, 
-                         HttpResponseForbidden,  
+from django.http import (HttpResponseNotFound,
+                         HttpResponseForbidden,
                          HttpResponseServerError)
 
 
