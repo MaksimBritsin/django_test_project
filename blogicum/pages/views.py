23855c7b3beafc405788248d58b5fef8606ace97
MaksimBritsin <111296865+MaksimBritsin@users.noreply.github.com>
2024-11-19 09:11:53 +0300

    Second

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -14,16 +14,20 @@ class RulesTemplateView(TemplateView):
 
 
 def permission_denied(request, exception):
-    return render(request, 'pages/403.html', status=HttpResponseForbidden.status_code)
+    return render(request, 'pages/403.html',
+     status=HttpResponseForbidden.status_code)
 
 
 def csrf_failure(request, reason=''):
-    return render(request, 'pages/403csrf.html', status=HttpResponseForbidden.status_code)
+    return render(request, 'pages/403csrf.html',
+        status=HttpResponseForbidden.status_code)
 
 
 def page_not_found(request, exception):
-    return render(request, 'pages/404.html', status=HttpResponseNotFound.status_code)
+    return render(request, 'pages/404.html', 
+        status=HttpResponseNotFound.status_code)
 
 
 def server_error(request):
-    return render(request, 'pages/500.html', status=HttpResponseServerError.status_code)
+    return render(request, 'pages/500.html', 
+        status=HttpResponseServerError.status_code)
